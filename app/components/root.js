017ae04def69c35a7139159538e8d1e56adac087
Patricia de Boer <pdeboer05@gmail.com>
2019-05-29 11:57:20 -0500

    All files for react before adding server elements

---
+++ app/components/root.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Root extends Component {
+  render() {
+    return (
+      <div>Hello from the Root file 😈</div>
+    )
+  }
+}
+
+export default Root
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app/main.js', // entry point is in app folder in main.js
+  mode: 'development',
+  output: {
+    path: __dirname, // bundle.js will also be in public folder within project
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
